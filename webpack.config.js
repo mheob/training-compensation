9b093ae57632b0040a555d6591c822da46ab52b1
Alexander Böhm <tools@boehm.work>
2019-06-24 12:17:19 +0200

    Reconfigure development environment

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: ['./app/src/index.js', './app/src/styles/styles.scss'],
+  entry: './app/src/index.js',
   output: {
     path: path.resolve(__dirname, 'app/dist/assets'),
     filename: 'bundle.js'
@@ -21,29 +21,6 @@ module.exports = {
             presets: ['@babel/preset-env']
           }
         }
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].css'
-            }
-          },
-          {
-            loader: 'extract-loader'
-          },
-          {
-            loader: 'css-loader?-url'
-          },
-          {
-            loader: 'postcss-loader'
-          },
-          {
-            loader: 'sass-loader'
-          }
-        ]
       }
     ]
   }
